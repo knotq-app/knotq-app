acd6be4a3c4f285306b3ab434e81a7a8ee2e80fb
enigmurl <43832426+enigmurl@users.noreply.github.com>
2026-09-07 19:49:37 -0400

    Make recurrence tests timezone independent

--- desktop/rrule/src/expand.rs
+++ desktop/rrule/src/expand.rs
@@ -97,10 +97,18 @@ pub(crate) fn materialize_occurrence(
 
 #[cfg(test)]
 mod tests {
-    use chrono::{TimeZone, Utc};
+    use chrono::{DateTime, Local, TimeZone, Utc};
     use knotq_model::{CalendarRecurrence, Item, OccurrenceId};
 
     use super::*;
+
+    fn local_utc(year: i32, month: u32, day: u32, hour: u32, minute: u32) -> DateTime<Utc> {
+        Local
+            .with_ymd_and_hms(year, month, day, hour, minute, 0)
+            .single()
+            .expect("test date must be representable in the local timezone")
+            .with_timezone(&Utc)
+    }
 
     #[test]
     fn non_recurring_event_in_range() {
@@ -118,44 +126,38 @@ mod tests {
     #[test]
     fn weekly_rrule_repeat_three_days_per_week() {
         let mut item = Item::new("MATH 15");
-        item.start = Some(Utc.with_ymd_and_hms(2026, 1, 5, 18, 0, 0).unwrap());
-        item.end = Some(Utc.with_ymd_and_hms(2026, 1, 5, 19, 0, 0).unwrap());
+        item.start = Some(local_utc(2026, 1, 5, 18, 0));
+        item.end = Some(local_utc(2026, 1, 5, 19, 0));
         item.repeats = Some(CalendarRecurrence {
             rrules: vec!["FREQ=WEEKLY;INTERVAL=1;BYDAY=MO,WE,FR".to_string()],
             ..Default::default()
         });
-        let occs = item.occurrences(
-            Utc.with_ymd_and_hms(2026, 1, 5, 0, 0, 0).unwrap(),
-            Utc.with_ymd_and_hms(2026, 1, 12, 0, 0, 0).unwrap(),
-        );
+        let occs = item.occurrences(local_utc(2026, 1, 5, 0, 0), local_utc(2026, 1, 12, 0, 0));
         assert_eq!(occs.len(), 3);
         assert_eq!(
             occs[1].id,
-            OccurrenceId::recurring_utc(Utc.with_ymd_and_hms(2026, 1, 7, 18, 0, 0).unwrap())
+            OccurrenceId::recurring_utc(local_utc(2026, 1, 7, 18, 0))
         );
     }
 
     #[test]
     fn weekly_rrule_without_byday_repeats_on_anchor_weekday() {
         let mut item = Item::new("review");
-        item.start = Some(Utc.with_ymd_and_hms(2026, 1, 8, 8, 30, 0).unwrap());
+        item.start = Some(local_utc(2026, 1, 8, 8, 30));
         item.repeats = Some(CalendarRecurrence {
             rrules: vec!["FREQ=WEEKLY;INTERVAL=1".to_string()],
             ..Default::default()
         });
 
-        let occs = item.occurrences(
-            Utc.with_ymd_and_hms(2026, 1, 1, 0, 0, 0).unwrap(),
-            Utc.with_ymd_and_hms(2026, 1, 23, 0, 0, 0).unwrap(),
-        );
+        let occs = item.occurrences(local_utc(2026, 1, 1, 0, 0), local_utc(2026, 1, 23, 0, 0));
 
         assert_eq!(occs.len(), 3);
         assert_eq!(
             occs.iter().filter_map(|occ| occ.start).collect::<Vec<_>>(),
             vec![
-                Utc.with_ymd_and_hms(2026, 1, 8, 8, 30, 0).unwrap(),
-                Utc.with_ymd_and_hms(2026, 1, 15, 8, 30, 0).unwrap(),
-                Utc.with_ymd_and_hms(2026, 1, 22, 8, 30, 0).unwrap(),
+                local_utc(2026, 1, 8, 8, 30),
+                local_utc(2026, 1, 15, 8, 30),
+                local_utc(2026, 1, 22, 8, 30),
             ]
         );
     }
@@ -163,67 +165,61 @@ mod tests {
     #[test]
     fn repeat_count_is_total_occurrences() {
         let mut item = Item::new("class");
-        item.start = Some(Utc.with_ymd_and_hms(2026, 1, 5, 18, 0, 0).unwrap());
+        item.start = Some(local_utc(2026, 1, 5, 18, 0));
         item.repeats = Some(CalendarRecurrence {
             rrules: vec!["FREQ=WEEKLY;INTERVAL=1;BYDAY=MO,WE,FR;COUNT=4".to_string()],
             ..Default::default()
         });
 
-        let occs = item.occurrences(
-            Utc.with_ymd_and_hms(2026, 1, 1, 0, 0, 0).unwrap(),
-            Utc.with_ymd_and_hms(2026, 1, 31, 0, 0, 0).unwrap(),
-        );
+        let occs = item.occurrences(local_utc(2026, 1, 1, 0, 0), local_utc(2026, 1, 31, 0, 0));
 
         assert_eq!(occs.len(), 4);
         assert_eq!(occs[0].occurrence_index, 0);
         assert_eq!(occs[3].occurrence_index, 3);
         assert_eq!(
             occs[3].id,
-            OccurrenceId::recurring_utc(Utc.with_ymd_and_hms(2026, 1, 12, 18, 0, 0).unwrap())
+            OccurrenceId::recurring_utc(local_utc(2026, 1, 12, 18, 0))
         );
     }
 
     #[test]
     fn date_only_until_includes_occurrences_on_that_date() {
         let mut item = Item::new("class");
-        item.start = Some(Utc.with_ymd_and_hms(2026, 1, 5, 18, 0, 0).unwrap());
+        item.start = Some(local_utc(2026, 1, 5, 18, 0));
         item.repeats = Some(CalendarRecurrence {
             rrules: vec!["FREQ=DAILY;INTERVAL=1;UNTIL=20260107".to_string()],
             ..Default::default()
         });
 
-        let occs = item.occurrences(
-            Utc.with_ymd_and_hms(2026, 1, 1, 0, 0, 0).unwrap(),
-            Utc.with_ymd_and_hms(2026, 1, 10, 0, 0, 0).unwrap(),
-        );
+        let occs = item.occurrences(local_utc(2026, 1, 1, 0, 0), local_utc(2026, 1, 10, 0, 0));
 
         assert_eq!(occs.len(), 3);
         assert_eq!(
             occs[2].id,
-            OccurrenceId::recurring_utc(Utc.with_ymd_and_hms(2026, 1, 7, 18, 0, 0).unwrap())
+            OccurrenceId::recurring_utc(local_utc(2026, 1, 7, 18, 0))
         );
     }
 
     #[test]
     fn recurring_occurrence_identity_is_stable_across_query_windows() {
         let mut item = Item::new("class");
-        item.start = Some(Utc.with_ymd_and_hms(2026, 1, 5, 18, 0, 0).unwrap());
-        item.end = Some(Utc.with_ymd_and_hms(2026, 1, 5, 19, 0, 0).unwrap());
+        item.start = Some(local_utc(2026, 1, 5, 18, 0));
+        item.end = Some(local_utc(2026, 1, 5, 19, 0));
         item.repeats = Some(CalendarRecurrence {
             rrules: vec!["FREQ=WEEKLY;INTERVAL=1;BYDAY=MO,WE,FR".to_string()],
             ..Default::default()
         });
 
-        let wed_start = Utc.with_ymd_and_hms(2026, 1, 7, 0, 0, 0).unwrap();
-        let wed_end = Utc.with_ymd_and_hms(2026, 1, 8, 0, 0, 0).unwrap();
-        let week_start = Utc.with_ymd_and_hms(2026, 1, 5, 0, 0, 0).unwrap();
-        let week_end = Utc.with_ymd_and_hms(2026, 1, 12, 0, 0, 0).unwrap();
+        let wed_start = local_utc(2026, 1, 7, 0, 0);
+        let wed_end = local_utc(2026, 1, 8, 0, 0);
+        let week_start = local_utc(2026, 1, 5, 0, 0);
+        let week_end = local_utc(2026, 1, 12, 0, 0);
 
         let day_id = item.occurrences(wed_start, wed_end)[0].id.clone();
         let week_id = item
             .occurrences(week_start, week_end)
             .into_iter()
-            .find(|occ| occ.start == Some(Utc.with_ymd_and_hms(2026, 1, 7, 18, 0, 0).unwrap()))
+            .find(|occ| occ.start == Some(local_utc(2026, 1, 7, 18, 0)))
             .unwrap()
             .id;
         assert_eq!(day_id, week_id);
