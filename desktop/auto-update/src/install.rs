23a5b6fbfceea5737f4db2ee5b5410818910d3cf
enigmurl <43832426+enigmurl@users.noreply.github.com>
2026-09-19 16:39:34 -0400

    Complete auto-update platform cfg cleanup

--- desktop/auto-update/src/install.rs
+++ desktop/auto-update/src/install.rs
@@ -1,6 +1,7 @@
 #[cfg(target_os = "macos")]
 use std::ffi::OsStr;
 use std::fs;
+#[cfg(target_os = "macos")]
 use std::io;
 use std::path::{Path, PathBuf};
 #[cfg(any(target_os = "macos", target_os = "linux"))]
